6d98e5832186ba59e17526f947221c1d8980c14e
Glynn Bird <glynn.bird@gmail.com>
2023-05-11 15:10:00 +0100

    it works if you ram everything into one file

--- cloudant.com/prometheus/main.go
+++ cloudant.com/prometheus/main.go
@@ -10,29 +10,49 @@ import (
 	//"collectors/replication"
 
 	// Cloudant Go SDK
+	"time"
+
 	"github.com/IBM/cloudant-go-sdk/cloudantv1"
 )
 
+func Collect(service *cloudantv1.CloudantV1) {
+
+	ticker := time.NewTicker(5000 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("Polling Cloudant replication", t)
+
+				getSchedulerDocsOptions := service.NewGetSchedulerDocsOptions()
+				schedulerDocsResult, _, _ := service.GetSchedulerDocs(getSchedulerDocsOptions)
+				b, _ := json.MarshalIndent(schedulerDocsResult, "", "  ")
+				fmt.Println(string(b))
+			}
+		}
+	}()
+
+}
 func main() {
 	fmt.Println("Hello, World!")
 
+	// connect to Cloudant
 	service, _ := cloudantv1.NewCloudantV1UsingExternalConfig(
 		&cloudantv1.CloudantV1Options{
 			ServiceName: "CLOUDANT",
 		})
 
-	getSchedulerDocsOptions := service.NewGetSchedulerDocsOptions()
-	schedulerDocsResult, _, _ := service.GetSchedulerDocs(getSchedulerDocsOptions)
-	b, _ := json.MarshalIndent(schedulerDocsResult, "", "  ")
-	//fmt.Println(string(b))
-
 	// collectors
 	// this doesn't work
-	//replication()
+	Collect(service)
 
 	// web server to handle Prometheus GET /metrics endpoint
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, string(b))
+		fmt.Fprintf(w, "metrics!")
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
