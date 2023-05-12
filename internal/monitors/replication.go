bf62433a0f6252285e3cd49d970b0dbefbe40e1f
Mike Rhodes <mike.rhodes@dx13.co.uk>
2023-05-12 17:23:52 +0100

    Full set of replication metrics per replication doc id
    Items:
    
    - changes pending
    - doc write failures
    - docs read
    - docs written
    - missing revs found
    - revs processed

--- internal/monitors/replication.go
+++ internal/monitors/replication.go
@@ -17,9 +17,39 @@ type ReplicationMonitor struct {
 }
 
 var (
-	docsProcessed = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "cloudant_replication_docs_processed_total",
-		Help: "The number of documents written to the target",
+	changesPendingTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudant_replication_changes_pending_total",
+		Help: "The number of changes remaining to process (approximately)",
+	},
+		[]string{"docid"},
+	)
+	docWriteFailuresTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudant_replication_doc_write_failures_total",
+		Help: "The number of failures writing documents to the target",
+	},
+		[]string{"docid"},
+	)
+	docsReadTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudant_replication_docs_read_total",
+		Help: "Total number of documents read from the source database",
+	},
+		[]string{"docid"},
+	)
+	docsWrittenTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudant_replication_docs_written_total",
+		Help: "Total number of documents written to the target database",
+	},
+		[]string{"docid"},
+	)
+	missingRevsFoundTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudant_replication_missing_revs_found_total",
+		Help: "Total number of revs found so far on the source that are not at the target",
+	},
+		[]string{"docid"},
+	)
+	revsCheckedTotal = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloudant_replication_revs_checked_total",
+		Help: "Total number of revs processed on the source",
 	},
 		[]string{"docid"},
 	)
@@ -62,7 +92,12 @@ func (rc *ReplicationMonitor) tick() error {
 	}
 	for _, d := range schedulerDocsResult.Docs {
 		log.Printf("ReplicationMonitor: Replication %q: docs written %d", *d.DocID, *d.Info.DocsWritten)
-		docsProcessed.WithLabelValues(*d.DocID).Set(float64(*d.Info.DocsWritten))
+		changesPendingTotal.WithLabelValues(*d.DocID).Set(float64(*d.Info.ChangesPending))
+		docWriteFailuresTotal.WithLabelValues(*d.DocID).Set(float64(*d.Info.DocWriteFailures))
+		docsReadTotal.WithLabelValues(*d.DocID).Set(float64(*d.Info.DocsRead))
+		docsWrittenTotal.WithLabelValues(*d.DocID).Set(float64(*d.Info.DocsWritten))
+		missingRevsFoundTotal.WithLabelValues(*d.DocID).Set(float64(*d.Info.MissingRevisionsFound))
+		revsCheckedTotal.WithLabelValues(*d.DocID).Set(float64(*d.Info.RevisionsChecked))
 	}
 	return nil
 }
