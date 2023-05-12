204e82bcc87ddfe6c40334fd4530e892a33e2e2e
Glynn Bird <glynn.bird@gmail.com>
2023-05-12 09:01:59 +0100

    plumb the polling of Cloudant metrics into the Prometheus output

--- cmd/couchmonitor/main.go
+++ cmd/couchmonitor/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
+
 	// "regexp"
 	"time"
 
@@ -22,33 +21,6 @@ import (
 
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
-// poll the Cloudant replication scheduler every 5 seconds
-func Collect(service *cloudantv1.CloudantV1) {
-
-	// nicked from https://gobyexample.com/tickers
-	ticker := time.NewTicker(5000 * time.Millisecond)
-	done := make(chan bool)
-
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case t := <-ticker.C:
-				fmt.Println("Polling Cloudant replication", t)
-
-				// fetch scheduler status
-				getSchedulerDocsOptions := service.NewGetSchedulerDocsOptions()
-				schedulerDocsResult, _, _ := service.GetSchedulerDocs(getSchedulerDocsOptions)
-				b, _ := json.MarshalIndent(schedulerDocsResult, "", "  ")
-
-				// to stdout - not plumbed into Prometheus client yet
-				fmt.Println(string(b))
-			}
-		}
-	}()
-}
-
 // entry point
 func main() {
 	log.Println("Hello, World!")
@@ -59,13 +31,10 @@ func main() {
 			ServiceName: "CLOUDANT",
 		})
 
-	// collectors - pass in the Cloudant service
-
-	Collect(service)
-
 	// Create a new registry.
 	reg := prometheus.NewRegistry()
 
+	// set up the replication collector to poll every 5s
 	rc := monitors.ReplicationCollector{
 		Reg:      reg,
 		Cldt:     service,
@@ -73,12 +42,6 @@ func main() {
 		Done:     make(chan bool),
 	}
 	rc.Go()
-
-	// Add Go module build info.
-	// reg.MustRegister(collectors.NewBuildInfoCollector())
-	// reg.MustRegister(collectors.NewGoCollector(
-	// 	collectors.WithGoCollectorRuntimeMetrics(collectors.GoRuntimeMetricsRule{Matcher: regexp.MustCompile("/.*")}),
-	// ))
 
 	// Expose the registered metrics via HTTP.
 	http.Handle("/metrics", promhttp.HandlerFor(
@@ -88,6 +51,5 @@ func main() {
 			EnableOpenMetrics: true,
 		},
 	))
-	log.Println("Hello world from new Go Collector!")
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
